709500ad7f839832ac5aa784e81a973e1dd70be4
Maria Gabrielly <ggabrielly.ggaby@gmail.com>
2022-05-27 21:49:43 -0300

    Atualizando

--- 6exercicio.js
+++ 6exercicio.js
@@ -1,3 +1,14 @@
 function inverso(valor){
-    
+    const v = valor.typeof() ;
+    if(v == Boolean){
+        if(valor == true){
+            return false;
+        } else{
+            return true;
+        }
+    } else if(v == Number){
+        return v*(-1);
+    } else{
+        return "booleano ou número esperados, mas o parâmetro é do tipo string";
+    }
 }
